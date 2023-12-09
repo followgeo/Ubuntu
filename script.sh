e51a2fc9c3bc132ae01726d0d824fc1e6e0060ed
followgeo <followgeo@gmail.com>
2023-12-09 06:22:04 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,23 +2,23 @@
 
 # Download xmrig-6.21.0-focal-x64.tar.gz
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-focal-x64.tar.gz
-sleep 15
+sleep 5
 # Unzip the downloaded file
 tar -xf xmrig-6.21.0-focal-x64.tar.gz
-sleep 15
+sleep 5
 # Change directory to the extracted folder
 cd xmrig-6.21.0
-sleep 15
+sleep 5
 # Download randomx_boost.sh and enable_1gb_pages.sh scripts
 wget https://raw.githubusercontent.com/xmrig/xmrig/dev/scripts/randomx_boost.sh
-sleep 13
+sleep 3
 wget https://raw.githubusercontent.com/xmrig/xmrig/dev/scripts/enable_1gb_pages.sh
-sleep 13
+sleep 3
 # Make the scripts executable
 chmod +x randomx_boost.sh enable_1gb_pages.sh
-sleep 13
+sleep 3
 # Execute the scripts with sudo privileges
 bash randomx_boost.sh
-sleep 13
+sleep 3
 bash enable_1gb_pages.sh
-sleep 13
+sleep 3
